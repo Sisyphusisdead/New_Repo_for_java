9316fab6811c21777804814d5a9a4532500e9a5b
Aman <upadhyayaman512@gmail.com>
2025-08-21 10:34:30 +0530

    polymorphism

---
+++ src/myshape.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class myshape {
+    public static void main(String args[]){
+        shape s = new other_shape();
+        s.draw(21);
+        s.draw(21,23);
+    }
+}
---
+++ src/other_shape.java
@@ -0,0 +1,6 @@
+public class other_shape extends shape {
+    public void draw(double l){
+        double area = l*l;
+        System.out.println("area of square of other shape "+area);
+    }
+}
